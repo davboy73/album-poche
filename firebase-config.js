e9f30b84c9ba39db61a08515cfe2bca656e5043b
David BOYER Photographe <DavidBOYERPhotographe@MacBookPro.lan>
2026-06-23 20:22:41 +0200

    Album Poche v1

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// ============================================
+// CONFIGURATION FIREBASE (Firestore)
+// ============================================
+const firebaseConfig = {
+  apiKey: "AIzaSyCxFF3BwEwZOmi31apxZKJY8pAdTQWjsMU",
+  authDomain: "album-poche-db.firebaseapp.com",
+  projectId: "album-poche-db",
+  storageBucket: "album-poche-db.firebasestorage.app",
+  messagingSenderId: "621142398477",
+  appId: "1:621142398477:web:1ee29583919bbb96b06744"
+};
+
+// ============================================
+// CONFIGURATION CLOUDINARY (stockage photos)
+// ============================================
+const cloudinaryConfig = {
+  cloudName: "ulwuajnp",
+  apiKey: "176484237194419",
+  uploadPreset: "album_poche"
+};
